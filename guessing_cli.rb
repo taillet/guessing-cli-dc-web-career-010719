35b20e39aa9715574e08655bce0df019d9c6e44e
Heloise Taillet <ht2394@columbia.edu>
2019-01-06 02:12:58 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -6,7 +6,7 @@ def run_guessing_game
 
 puts "Guess a number between 1 and 6."
 guess = gets.chomp
-while guess != exit
+while guess != "exit"
   number = Random.rand(1...6)
     if guess.to_i == number
       puts "You guessed the correct number!"
