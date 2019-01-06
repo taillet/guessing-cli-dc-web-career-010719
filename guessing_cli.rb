d4087e4dcaeda8acadc66c33f17e7f7bd0bc56a5
Heloise Taillet <ht2394@columbia.edu>
2019-01-06 01:59:07 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -9,14 +9,5 @@ guess = gets.chomp
 number = Random.rand(1...6)
 
 
-if guess == exit
-  puts "Goodbye"
-end
-
-    if guess == number
-      puts "You guessed the correct number!"
-    else
-      puts "The computer guessed #{number}."
-    end
 
 end
