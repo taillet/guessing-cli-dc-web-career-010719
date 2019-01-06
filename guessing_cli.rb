1dcd126dd310a1525dc4702383204ea331c771d1
Heloise Taillet <ht2394@columbia.edu>
2019-01-06 02:44:24 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -9,9 +9,8 @@ question
 guess = gets.chomp
 number = rand(1..6)
 
-  if guess != "exit" and (1..6).include?(guess.to_i) == false
-    question
-    guess = gets.chomp
+  if guess == "exit"
+    exit_game
   else
     if guess.to_i == number
       puts "You guessed the correct number!"
