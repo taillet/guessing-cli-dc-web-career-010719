6381375b869e9d2cb7928c8b944337e3ae21c720
Heloise Taillet <ht2394@columbia.edu>
2019-01-06 02:52:46 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,24 +5,21 @@ def question
 end
 
 def run_guessing_game
+
+  loop do
 question
 guess = gets.chomp
 number = rand(1..6)
-
-while guess != exit do
-  if guess.to_i == number
+  if guess == "exit"
+    exit_game
+    break
+  elsif guess.to_i == number
       puts "You guessed the correct number!"
     elsif guess.to_i != number
       puts "The computer guessed #{number}."
     else
       "Try again."
     end
-    number = rand(1..6)
-    guess = gets.chomp
-end
-
-  if guess == "exit"
-    exit_game
   end
 end
 
