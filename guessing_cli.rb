60e2bacda6cecb14b09a59aaf48d678c38917ffa
Heloise Taillet <ht2394@columbia.edu>
2019-01-06 02:29:05 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -7,13 +7,15 @@ def question
 end
 
 def run_guessing_game
+
+  if guess == "exit"
+    exit_game
+  end
 question
 number = Random.rand(1..6)
 guess = gets.chomp
 
-if guess == "exit"
-  exit_game
-end
+
 
 if guess != "exit" and (1..6).include?(guess.to_i) == false
   question
