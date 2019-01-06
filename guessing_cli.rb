f33c0adcc637d064bd03bf68d41d75916b07b06e
Heloise Taillet <ht2394@columbia.edu>
2019-01-06 02:24:55 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -10,12 +10,18 @@ def run_guessing_game
 question
 number = Random.rand(1..6)
 guess = gets.chomp
+
+if (1..6).include?(guess) == false
+  question
+  guess = gets.chomp
+else
   if guess.to_i == number
     puts "You guessed the correct number!"
   elsif guess.to_i != number
     puts "The computer guessed #{number}."
   elsif guess == "exit"
     exit_game
+end
 end
 
 def exit_game
