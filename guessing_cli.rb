381bb282919c74da2ab17e43798287179e5a8c25
Heloise Taillet <ht2394@columbia.edu>
2019-01-06 02:09:34 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -8,9 +8,9 @@ puts "Guess a number between 1 and 6."
 guess = gets.chomp
 while guess != exit
   number = Random.rand(1...6)
-    if guess == number
+    if guess.to_i == number
       puts "You guessed the correct number!"
-    elsif guess != number
+    elsif guess.to_i != number
       puts "The computer guessed #{number}."
     elsif guess == "exit"
       puts "Goodbye"
