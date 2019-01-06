64d255aa403cf675d6f452f3503ce1b5edd1bdde
Heloise Taillet <ht2394@columbia.edu>
2019-01-06 02:58:46 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -11,7 +11,6 @@ def run_guessing_game
     number = rand(1..6)
     if guess == "exit"
       exit_game
-      break
     elsif guess.to_i == number
       puts "You guessed the correct number!"
     elsif guess.to_i != number
